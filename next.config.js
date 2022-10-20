ae5b3ea55a38df1f1983254aee9f8044060af9ce
svensk007 <83252177+svensk007@users.noreply.github.com>
2022-10-20 14:42:27 -0500

    add loader

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
   basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs=pages"
+  assetPrefix: "/nextjs=pages",
 }
 
 module.exports = nextConfig
