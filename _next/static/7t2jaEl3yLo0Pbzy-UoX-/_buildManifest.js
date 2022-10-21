5ed8d9fa67745b76f326b7a31ea0d1010181784f
svensk007 <83252177+svensk007@users.noreply.github.com>
2022-10-21 00:47:20 +0000

    Deploying to gh-pages from @ svensk007/svensk007.github.io@895d6b2a75f7bbeec3bfca69fe36eda28bdbd0a1 🚀

---
+++ _next/static/7t2jaEl3yLo0Pbzy-UoX-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-45c972a4aba6ac61.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/explore":["static/chunks/pages/explore-d9be1904d8ec9287.js"],sortedPages:["/","/_app","/_error","/explore"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
