767ce7f0217e48f8af01e50e49e24cccacaf9529
svensk007 <83252177+svensk007@users.noreply.github.com>
2022-10-21 06:09:34 -0500

    formatting

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -9,32 +9,60 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='font-Inter bg-gray-850'>
-        <section className='min-h-screen'>
-
-          <nav className='py-8 px-8 mb-20 flex justify-between bg-gradient-to-b from-black/60 to-white/0'>
-            <h1 className='cursor-pointer font-extrabold text-5xl text-white hover:underline' href="./index.js"><span className='text-transparent bg-clip-text bg-gradient-to-br from-hot-pink to-regal-blue'>El3troNam </span>Studios</h1>
-            <ul className='flex items-center'>
-              <li><a className='text-white text-2xl px-4 ml-4 hover:underline' href='./contact'>Contact Us</a></li>
+      <main className="font-Inter bg-gray-850">
+        <section className="min-h-screen">
+          <nav className="py-8 px-8 mb-20 flex justify-between bg-gradient-to-b from-black/60 to-white/0">
+            <h1
+              className="cursor-pointer font-extrabold text-5xl text-white hover:underline"
+              href="./index.js"
+            >
+              <span className="text-transparent bg-clip-text bg-gradient-to-br from-hot-pink to-regal-blue">
+                El3troNam{" "}
+              </span>
+              Studios
+            </h1>
+            <ul className="flex items-center">
+              <li>
+                <a
+                  className="text-white text-2xl px-4 ml-4 hover:underline"
+                  href="./contact"
+                >
+                  Contact Us
+                </a>
+              </li>
             </ul>
           </nav>
 
-          <div className='px-36'>
-
-            <div className='pt-36'>
-              <h2 className='text-8xl font-extrabold text-white'>&lsquo;ello there!👋</h2>
-              <h3 className='text-white text-4xl font-semibold'>We <span className='line-through'> are</span> <span className='text-slate-600/90'>do minimal amounts of trolling</span></h3>
+          <div className="px-36">
+            <div className="pt-36">
+              <h2 className="text-8xl font-extrabold text-white">
+                &lsquo;ello there!👋
+              </h2>
+              <h3 className="text-white text-4xl font-semibold">
+                We <span className="line-through"> are</span>{" "}
+                <span className="text-slate-600/90">
+                  do minimal amounts of trolling
+                </span>
+              </h3>
             </div>
 
-            <div className='pt-10'>
-              <button type='button' className='transition-all duration-150 ease-linear bg-hot-pink text-white px-7 py-2.5 rounded-md hover:drop-shadow-lg hover:bg-regal-blue hover:scale-105 focus:outline-none focus:ring focus:ring-fuchsia-900'>Explore</button>
-              <button type='button' className='px-8 py-3 ml-2 text-gray-400 outline-none hover:underline hover:text-white transition-all duration-75'>Learn More</button>
+            <div className="pt-10">
+              <button
+                type="button"
+                className="transition-all duration-150 ease-linear bg-hot-pink text-white px-7 py-2.5 rounded-md hover:drop-shadow-lg hover:bg-regal-blue hover:scale-105 focus:outline-none focus:ring focus:ring-fuchsia-900"
+              >
+                Explore
+              </button>
+              <button
+                type="button"
+                className="px-8 py-3 ml-2 text-gray-400 outline-none hover:underline hover:text-white transition-all duration-75"
+              >
+                Learn More
+              </button>
             </div>
-
           </div>
-
         </section>
       </main>
     </div>
-  )
+  );
 }
